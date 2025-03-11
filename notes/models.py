d11a86902a6d245d942152b4fb273c67db086325
Haroon <haroonhsa1234@gmail.com>
2025-03-11 07:04:42 +0500

    feat: Add 'is_deleted' field to Notes model

--- notes/models.py
+++ notes/models.py
@@ -9,6 +9,8 @@ class Notes(models.Model):
 
     likes = models.PositiveIntegerField(default=0)
 
+    is_deleted = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.title
 
