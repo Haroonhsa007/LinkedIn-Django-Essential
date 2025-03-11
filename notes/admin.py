7d165a94626199e08588f4159b7fe803762896bd
Haroon <haroonhsa1234@gmail.com>
2025-03-11 06:36:23 +0500

    feat: Add NotesAdmin class to admin.py for improved management of notes

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'created_at', 'updated_at', 'likes')
+
+admin.site.register(models.Notes, NotesAdmin)
