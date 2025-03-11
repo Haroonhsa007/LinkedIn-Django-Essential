135d2e565bdede050e7548bd80e22ca0995a9ca6
Haroon <haroonhsa1234@gmail.com>
2025-03-11 07:04:57 +0500

    refactor: Update list view in views.py to use Notes model for retrieving and displaying notes

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Notes
+
+
+def list(request):
+    notes = Notes.objects.filter(is_deleted=False).order_by('-created_at')
+
+    context = {
+        "notes": notes
+    }
+
+    return render(request, "notes/notes_list.html", context)
