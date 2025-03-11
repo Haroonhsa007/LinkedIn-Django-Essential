460d2422e63f68c23ba4224bda09fdd6277ae75d
Haroon <haroonhsa1234@gmail.com>
2025-03-11 07:04:25 +0500

    feat: Update NotesAdmin class in admin.py to display 'is_deleted' field

--- notes/admin.py
+++ notes/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import models
 
 class NotesAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'created_at', 'updated_at', 'likes')
+    list_display = ('title','is_deleted' , 'created_at', 'updated_at', 'likes')
 
 admin.site.register(models.Notes, NotesAdmin)
