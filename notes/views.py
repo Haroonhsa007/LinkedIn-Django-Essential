139ce3b809e34b6ed0de06e700bd8d6551d99b76
Haroon <haroonhsa1234@gmail.com>
2025-03-12 21:32:39 +0500

    refactor: Update NotesListView to filter notes by 'is_deleted' and order by 'created_at' descending

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound
 from django.views.generic import DetailView, ListView
 
 from .models import Notes
@@ -9,7 +8,7 @@ class NotesListView(ListView):
     model = Notes
     template_name = "notes/notes_list.html"
     context_object_name = "notes"
-    # option1 
+    # option1
     queryset = Notes.objects.filter(is_deleted=False).order_by("-created_at")
 
     # option2
@@ -32,4 +31,6 @@ class PopularNotesListView(ListView):
     context_object_name = "popular_notes"
 
     def get_queryset(self):
-        return Notes.objects.filter(is_deleted=False, likes__gte=1).order_by("-created_at")
+        return Notes.objects.filter(is_deleted=False, likes__gte=1).order_by(
+            "-created_at"
+        )
