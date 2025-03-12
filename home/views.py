bc13091803e5aee6709395695157f27e0474abfb
Haroon <haroonhsa1234@gmail.com>
2025-03-12 22:10:56 +0500

    refactor: Update home view to include current date in the context

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.utils import timezone
+from django.utils.dateformat import format
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_date'] = format(timezone.now(), 'Y-m-d H:i:s')
+        return context
